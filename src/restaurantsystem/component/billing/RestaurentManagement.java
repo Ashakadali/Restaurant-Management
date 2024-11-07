6766eb351cb38d8e8809cc6ef7ab6b41ded9688c
Asha Kadali <141851445+Ashakadali@users.noreply.github.com>
2024-11-07 22:30:46 +0530

    Add files via upload

---
+++ src/restaurantsystem/component/billing/RestaurentManagement.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package restaurantsystem.component.billing;
+
+/**
+ *
+ * @author Prasad Kadali
+ */
+class RestaurentManagement {
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
